96e8f2c89276a9185918964c96e1f4237f3bf578
Fausto Sgobba <fausto.sgobba@leica-geosystems.com>
2021-04-07 10:59:20 +0200

    PEF-46 - text-to-speech-offline works but with limited support on language in safari mobile

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
+import TTS from 'text-to-speech-offline'
 
 function App() {
+
+  const speak = ()=>{
+    console.log('speak');
+    // TTS(string, language, volume, rate, pitch)
+    TTS('Ciao Fausto, come stai?','it-IT')
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Test Text to speech</h1>
+      <button onClick={speak}>Speak</button>
     </div>
   );
 }
