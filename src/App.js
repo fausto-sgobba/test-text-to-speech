f5e5e9a326f83c10bb18fed5b568bd2ad1ee4712
Fausto Sgobba <fausto.sgobba@leica-geosystems.com>
2021-04-07 12:34:00 +0200

    PEF-46 - talkify.min.js

--- src/App.js
+++ src/App.js
@@ -1,18 +1,32 @@
 import './App.css';
-import TTS from 'text-to-speech-offline'
+import TTS from 'text-to-speech-offline';
+require('talkify-tts/dist/talkify.min.js');
 
 function App() {
-
-  const speak = ()=>{
-    console.log('speak');
+  const textToSpeak =
+    'Prosegui per 300 metri, alla rotonda prendi la seconda uscita';
+  const speakLibrary1 = () => {
+    console.log('speak 1');
     // TTS(string, language, volume, rate, pitch)
-    TTS('Ciao Fausto, come stai?','it-IT')
-  }
+    TTS(textToSpeak, 'it-IT');
+  };
+
+  const speakLibrary2 = () => {
+    console.log('speak 2');
+    window.responsiveVoice.speak(textToSpeak, 'Italian Female');
+  };
 
   return (
-    <div className="App">
+    <div className='App'>
       <h1>Test Text to speech</h1>
-      <button onClick={speak}>Speak</button>
+      <div className='row'>
+        <button onClick={speakLibrary1}>
+          Speak with text-to-speech-offline
+        </button>
+      </div>
+      <div className='row'>
+        <button onClick={speakLibrary2}>Speak with responsivevoice.org</button>
+      </div>
     </div>
   );
 }
