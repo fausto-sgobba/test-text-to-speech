a4c8cfa9b17894cd63ece7b677a9b975533e684d
Fausto Sgobba <fausto.sgobba@leica-geosystems.com>
2021-04-07 15:13:41 +0200

    PEF-46 - some docs

--- src/App.js
+++ src/App.js
@@ -49,27 +49,42 @@ function App() {
   return (
     <div className='App'>
       <h1>Test Text to speech</h1>
+      <p className='desc'>
+        The application tries to speak with TTS after 2s and with
+        ResponsiveVoice after 4s automatically
+      </p>
+      <h2>Interaction tests</h2>
       <div className='row'>
         <button onClick={speakLibraryTTS}>
           Speak with text-to-speech-offline
         </button>
+        <p className='desc'>Simple use with click</p>
       </div>
       <div className='row'>
         <button onClick={speakLibraryResponsiveVoice}>
           Speak with responsivevoice.org
         </button>
+        <p className='desc'>Simple use with click</p>
       </div>
       <div className='row'>
         <button onClick={noInteraction}>With timeout</button>
+        <p className='desc'>
+          Voice with TTS after 2s, voice with ResponsiveVoice after 4s
+        </p>
       </div>
       <div className='row'>
         <button onClick={subsequentVoicesTTS}>Subsequent voices TTS</button>
+        <p className='desc'>Different voice every 2,5s</p>
       </div>
       <div className='row'>
         <button onClick={subsequentVoicesReesponsiveVoice}>
           Subsequent voices Responsive Voice
         </button>
+        <p className='desc'>Different voice every 2,5s</p>
       </div>
+      <h2>References</h2>
+      <p className='desc'>https://github.com/faktaarief/text-to-speech-off</p>
+      <p className='desc'>https://responsivevoice.org/</p>
     </div>
   );
 }
