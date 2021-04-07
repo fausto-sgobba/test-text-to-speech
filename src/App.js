17a68bb97be3c2750f052c7780960cf9914d510b
Fausto Sgobba <fausto.sgobba@leica-geosystems.com>
2021-04-07 14:59:20 +0200

    PEF-46 - test with subsequent voices

--- src/App.js
+++ src/App.js
@@ -6,13 +6,13 @@ require('talkify-tts/dist/talkify.min.js');
 function App() {
   const textToSpeak =
     'Prosegui per 300 metri, alla rotonda prendi la seconda uscita';
-  const speakLibrary1 = () => {
+  const speakLibraryTTS = () => {
     console.log('speak 1');
     // TTS(string, language, volume, rate, pitch)
     TTS(textToSpeak, 'it-IT');
   };
 
-  const speakLibrary2 = () => {
+  const speakLibraryResponsiveVoice = () => {
     console.log('speak 2');
     window.responsiveVoice.speak(textToSpeak, 'Italian Female');
   };
@@ -26,6 +26,22 @@ function App() {
     }, 4000);
   };
 
+  const directions = [
+    'Prosegui 100 metri fino alla rotonda',
+    'Prendi la prima uscita a destra',
+    'Tieni la destra',
+    'Destinazione raggiunta',
+  ];
+  const subsequentVoicesTTS = () => {
+    const speak = (text) => TTS(text, 'it-IT');
+    directions.forEach((d, i) => setTimeout(() => speak(d), 2500 * i));
+  };
+  const subsequentVoicesReesponsiveVoice = () => {
+    const speak = (text) =>
+      window.responsiveVoice.speak(text, 'Italian Female');
+    directions.forEach((d, i) => setTimeout(() => speak(d), 2500 * i));
+  };
+
   useEffect(() => {
     noInteraction();
   }, []);
@@ -34,15 +50,25 @@ function App() {
     <div className='App'>
       <h1>Test Text to speech</h1>
       <div className='row'>
-        <button onClick={speakLibrary1}>
+        <button onClick={speakLibraryTTS}>
           Speak with text-to-speech-offline
         </button>
       </div>
       <div className='row'>
-        <button onClick={speakLibrary2}>Speak with responsivevoice.org</button>
+        <button onClick={speakLibraryResponsiveVoice}>
+          Speak with responsivevoice.org
+        </button>
       </div>
       <div className='row'>
         <button onClick={noInteraction}>With timeout</button>
+      </div>
+      <div className='row'>
+        <button onClick={subsequentVoicesTTS}>Subsequent voices TTS</button>
+      </div>
+      <div className='row'>
+        <button onClick={subsequentVoicesReesponsiveVoice}>
+          Subsequent voices Responsive Voice
+        </button>
       </div>
     </div>
   );
